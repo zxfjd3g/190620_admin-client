38f18c49c53755702a04407a3a2b88bc2f098da8
zxfjd3g <258147149@qq.com>
2019-10-24 15:47:14 +0800

    子路由

---
+++ src/containers/admin/header/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './index.less'
+/* 
+管理界面的头部组件
+*/
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        Header
+      </div>
+    )
+  }
+}
--- src/containers/admin/index.jsx
+++ src/containers/admin/index.jsx
@@ -2,38 +2,55 @@
 后台管理的一级路由组件
 */
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import {Redirect} from 'react-router-dom'
+import { Layout } from 'antd'
+import {Route, Switch, Redirect} from 'react-router-dom'
 
-import { removeUserToken } from '../../redux/action-creators/user'
-import {reqUsers} from '../../api'
+import LeftNav from './left-nav'
+import AdminHeader from './header'
 import WithCheckLogin from '../with-check-login'
 
-@connect(
-  state => ({user: state.user.user}),
-  {removeUserToken}
-)
+import Home from '../../components/home'
+import Category from '../category'
+import Product from '../product'
+import Role from '../role'
+import User from '../user'
+import Line from '../../components/charts/line'
+import Bar from '../../components/charts/bar'
+import Pie from '../../components/charts/pie'
+
+
+
+const { Header, Footer, Sider, Content } = Layout
+
 @WithCheckLogin
 class Admin extends Component {
 
-  logout = () => {
-    this.props.removeUserToken()
-  }
-
-  getUsers = async () => {
-    reqUsers()
-    const result = await reqUsers()
-    console.log('result', result)
-  }
+ 
 
   render() {
     return (
-      <div>
-        <p>Hello, {this.props.user.username}</p>
-        <button onClick={this.logout}>退出登陆2</button>
-        &nbsp;
-        <button onClick={this.getUsers}>获取用户列表</button>
-      </div>
+      <Layout style={{height: '100%'}}>
+        <Sider>
+          <LeftNav/>
+        </Sider>
+        <Layout>
+          <AdminHeader/>
+          <Content style={{backgroundColor: 'white', margin: '30px 15px 0 15px'}}>
+            <Switch>
+              <Route path="/home" component={Home}/>
+              <Route path="/category" component={Category}/>
+              <Route path="/product" component={Product}/>
+              <Route path="/role" component={Role}/>
+              <Route path="/user" component={User}/>
+              <Route path="/charts/line" component={Line}/>
+              <Route path="/charts/bar" component={Bar}/>
+              <Route path="/charts/pie" component={Pie}/>
+              <Redirect to="/home"/>
+            </Switch>
+          </Content>
+          <Footer style={{textAlign: 'center'}}>推荐使用谷歌浏览器，可以获得更佳页面操作体验</Footer>
+        </Layout>
+      </Layout>
     )
   }
 }
