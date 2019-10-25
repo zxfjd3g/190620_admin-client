f73abd123bd558de599e2ab2fbc07bc6c1d88620
zxfjd3g <258147149@qq.com>
2019-10-25 10:32:37 +0800

    jsonp 天气

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,8 @@
 函数的返回值是promise
 */
 import ajax from './ajax'
+import jsonp from 'jsonp'
+import {message} from 'antd'
 
 /* 登陆 */
 export const reqLogin = ({username, password}) => ajax({
@@ -18,3 +20,29 @@ export const reqUsers = () => ajax({
 })
 // ajax('/manage/user/list')
 // ajax.get('/manage/user/list')
+
+/* 
+封装获取指定城市的天气信息
+使用jsonp请求
+*/
+
+export const reqWeather = (city) => {
+
+  return new Promise((resolve, reject) => {
+    const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+    // callback函数由jsonp库内部定义__jp0函数调用的 --> callback参数来告诉服务器返回的js代码: __jp(data)
+    jsonp(url, {}, (err, data) => { 
+      console.log(err, data)
+      if (!err && data.status==='success') {
+        const {dayPictureUrl, weather} = data.results[0].weather_data[0]
+        resolve({dayPictureUrl, weather})
+      } else {
+        // reject(new Error('获取天气失败!'))
+        message.error('获取天气失败!')
+        return new Promise(() => {})
+      }
+    })
+  })
+  
+}
+// reqWeather('北京')
--- src/containers/admin/header/index.jsx
+++ src/containers/admin/header/index.jsx
@@ -1,26 +1,53 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'  // 高阶组件, 用来包装非路由组件
+import { Modal } from 'antd';
 import dayjs from 'dayjs'
 import format from 'date-fns/format'
 
+import {removeUserToken} from '../../../redux/action-creators/user'
 import LinkButton from '../../../components/link-button'
+import {reqWeather} from '../../../api'
 
 import './index.less'
 /* 
 管理界面的头部组件
 */
-@connect(state => ({username: state.user.user.username}))
+@connect(
+  state => ({username: state.user.user.username}),
+  {removeUserToken}
+)
 @withRouter  // 向组件内部传入3个属性: history/location/match
 class Header extends Component {
 
   state = {
     // currentTime: dayjs().format('YYYY-MM-DD HH:mm:ss')
-    currentTime: format(Date.now(), 'yyyy-MM-dd HH:mm:ss')
+    currentTime: format(Date.now(), 'yyyy-MM-dd HH:mm:ss'),
+    dayPictureUrl: '',  // 天气图片的url
+    weather: '' // 天气文本
   }
 
   logout = () => {
-    alert('logout')
+    // 显示确认框
+    Modal.confirm({
+      title: '确认退出吗?',
+      onOk: () => {
+        this.props.removeUserToken()
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    })
+  }
+
+  showWeather = async () => {
+    // 请求获取数据
+    const {dayPictureUrl, weather} = await reqWeather('北京')
+    // 更新状态
+    this.setState({
+      dayPictureUrl, 
+      weather
+    })
   }
 
 
@@ -31,6 +58,8 @@ class Header extends Component {
         currentTime: dayjs().format('YYYY-MM-DD HH:mm:ss')
       })
     }, 1000);
+    // 请求获取天气信息显示
+    this.showWeather()
   }
 
   componentWillUnmount () {
@@ -42,7 +71,7 @@ class Header extends Component {
   render() {
     // 得到当前请求的路由路径
     const path = this.props.location.pathname
-    const {currentTime} = this.state
+    const {currentTime, dayPictureUrl, weather} = this.state
 
     return (
       <div className="header">
@@ -54,8 +83,8 @@ class Header extends Component {
           <div className="header-bottom-left">{path}</div>
           <div className="header-bottom-right">
             <span>{currentTime}</span>
-            <img src="http://api.map.baidu.com/images/weather/day/xiaoyu.png" alt="weather"/>
-            <span>小雨转多云</span>
+            <img src={dayPictureUrl} alt="weather"/>
+            <span>{weather}</span>
           </div>
         </div>
       </div>
